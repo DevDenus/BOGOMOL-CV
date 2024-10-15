209f3bc79fbf9899afb61948c98bf79d7dc1b5e4
DevDenus <p7rt4535646566@gmail.com>
2024-10-16 01:30:18 +0300

    Changed binary multiplication into integer multiplication mod 2

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-# TODO: Apply effective matrix multiplication
+# TODO: Apply effective matrix multiplication using
+# XOR and AND as summation and multiplication
 def bin_matmul(mat_l : np.array, mat_r : np.array) -> np.array:
     """
     Implementation of binary matrix multiplication, using
@@ -9,14 +10,7 @@ def bin_matmul(mat_l : np.array, mat_r : np.array) -> np.array:
     mat_r : np.array - right matrix
     """
     assert mat_l.shape[1] == mat_r.shape[0], "Matrix dimensions doesn't match!"
-    if not mat_l.dtype == mat_r.dtype == np.dtype('bool_'):
-        mat_l, mat_r = np.array(mat_l, dtype=np.bool_), np.array(mat_r, dtype=np.bool_)
-    result = np.array(
-        [
-            [
-                np.count_nonzero(np.logical_and(mat_l[i, :], mat_r[:, j])) % 2 for j in range(mat_r.shape[1])
-            ] for i in range(mat_l.shape[0])
-        ],
-        dtype=np.bool_
-    )
-    return result
+    if not mat_l.dtype == mat_r.dtype == np.dtype('int_'):
+        mat_l, mat_r = np.array(mat_l, dtype=np.int_), np.array(mat_r, dtype=np.int_)
+    result = mat_l @ mat_r % 2
+    return np.array(result, dtype=np.bool_)
